b4cbdaf4584544331cf1835061e9c15c14e4f40c
Lluis Mather <lluis@mathergroup.io>
2023-04-08 12:40:09 +0200

    dotenv support and new secret key

--- worker.py
+++ worker.py
@@ -1,10 +1,10 @@
 import os
 import django
 from django.conf import settings
-from bookings import job
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aimharder.settings')
 django.setup()
 settings.DEBUG = False
 
+from bookings import job
 job.run()
